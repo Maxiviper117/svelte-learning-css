9e04770d69a118490bf1b9642b8b6ed0daab56c5
David <davidgonsal@gmail.com>
2023-10-14 17:25:48 +0200

    add: CssSubGrid example using columns

---
+++ src/lib/components/css_grid/CssSubGrid.md
@@ -0,0 +1,109 @@
+# [Easy and more consistent layouts using subgrid](https://youtu.be/IIQa9f0REtM)
+
+
+### CSS Grid and Subgrid Introduction
+
+CSS Grid Layout provides a two-dimensional grid-based layout system for web pages, allowing you to define columns and rows for your web content. The introduction of **subgrid** in the CSS Grid specification allows nested grids (child elements) to inherit the grid definition from their parent grid container. 
+
+### Basic Grid Usage
+
+Let's establish the basics of using a grid layout:
+
+```css
+.container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+}
+```
+```html
+<div class="container">
+  <div>Item 1</div>
+  <div>Item 2</div>
+  <div>Item 3</div>
+  <!-- More Items -->
+</div>
+```
+Here, `.container` is a grid with three equal-width columns. `grid-gap: 20px;` defines the space between grid items.
+
+### Subgrid Usage
+
+The subgrid feature allows a nested grid to use the grid tracks defined by its parent grid container. This ensures that the nested grid’s items align with the parent grid’s lines.
+
+Here's an example of how subgrid might be used:
+
+```css
+.container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+}
+
+.item {
+  display: grid;
+  grid-template-columns: subgrid;
+  grid-gap: 5px;
+}
+```
+```html
+<div class="container">
+  <div class="item">
+    <div>Subitem 1</div>
+    <div>Subitem 2</div>
+    <div>Subitem 3</div>
+  </div>
+  <!-- More Items -->
+</div>
+```
+In the example above, `.item` will have its columns aligned with `.container`’s columns, thanks to `grid-template-columns: subgrid;`. This enables a more consistent and aligned design when dealing with nested grids.
+
+### Additional Subgrid Usage Note
+
+Subgrid can also be used with rows, or both columns and rows, depending on the desired alignment and design:
+
+- `grid-template-columns: subgrid;` aligns columns with the parent.
+- `grid-template-rows: subgrid;` aligns rows with the parent.
+- Using both will align both rows and columns with the parent.
+
+### Practical Use Case: Card Layout
+
+Imagine a set of cards with a title, image, and button, and we want the button to be aligned at the bottom of each card, regardless of content size:
+
+```css
+.card-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+}
+
+.card {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+}
+
+.button {
+  grid-row: 3;
+}
+```
+```html
+<div class="card-container">
+  <div class="card">
+    <h2>Title</h2>
+    <img src="image.jpg" alt="Image"/>
+    <button class="button">Click me</button>
+  </div>
+  <!-- More Cards -->
+</div>
+```
+This CSS and HTML structure allows card content to take up as much space as it needs, while ensuring that buttons are consistently placed at the bottom of each card.
+
+### Considerations
+
+- Browser compatibility: At the time of the last training data in January 2022, subgrid was not universally supported across all major browsers. Always check for the most recent compatibility updates [here](https://caniuse.com/css-subgrid).
+  
+- Subgrid works well for designs requiring alignment between parent and child grids, but be mindful of content sizes and ensure that your design is flexible and accessible.
+
+- It's worthwhile to use fallbacks or alternative layouts for browsers that do not support subgrid to ensure that your website is usable and looks good across all platforms and browsers.
+
+Feel free to adjust and play around with this code, creating variations to help deepen your understanding of how CSS Grid and subgrid work!
+
---
+++ src/lib/components/css_grid/CssSubGrid.svelte
@@ -0,0 +1,313 @@
+<h1>
+    Change window width to see the difference in how the images resize
+    themselves
+</h1>
+<h3>
+    Each card is a grid with 2 columns, the first column is the image, the
+    second is the text. All cards are inside another parent grid with 4 columns.
+    Each card spans 2 columns. 
+    <br>
+    <br>
+    This is important for the subgrid, that the element spans 2 or more columns, in order for the child columns
+    to line up with the parent.
+</h3>
+<hr />
+<h1>With Subgrid</h1>
+<h3>
+    With subgrid, each card child grid respects the parent grid columns.
+    Meaning that the images all retain same width throughout the window resizing.
+</h3>
+<div class="with-subgrid card-grid">
+    <div class="card">
+        <h2>Lorem ipsum</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            recusandae maxime distinctio quos ullam incidunt.
+        </p>
+        <img src="//unsplash.it/501" alt="" />
+    </div>
+    <div class="card">
+        <h2>Omnis, veritatis odio.</h2>
+        <p>
+            Enim molestiae recusandae ut dolor sit amet consectetur adipisicing.
+            odit possimus eius inventore. Quasi illo unde neque? Itaque,
+            deleniti adipisci?
+        </p>
+        <img src="//unsplash.it/502" alt="" />
+    </div>
+    <div class="card">
+        <h2>Exercitationem, libero quam!</h2>
+        <p>
+            Error maiores culpa eaque. Quam quisquam quae nostrum ipsa dolorum
+            atque aperiam fugit soluta error!
+        </p>
+        <img src="//unsplash.it/503" alt="" />
+    </div>
+    <div class="card">
+        <h2>Quibusdam, recusandae odio.</h2>
+        <p>
+            Corporis laboriosam, neque est commodi architecto voluptatem, ipsam
+            corrupti ullam similique eligendi hic qui natus?
+        </p>
+        <img src="//unsplash.it/504" alt="" />
+    </div>
+    <div class="card">
+        <h2>Modi, exer citat ionem dicta.</h2>
+        <p>
+            Laboriosam, aliquam tempore minus dolorem ullam et veniam
+            asperiores, eveniet vitae odit itaque eligendi ducimus?
+        </p>
+        <img src="//unsplash.it/505" alt="" />
+    </div>
+    <div class="card">
+        <h2>Dolore.</h2>
+        <p>
+            Reprehenderit, labore. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Sunt, aut. harum vitae voluptate alias saepe
+            debitis quo perspiciatis, delectus fugiat modi eveniet.
+        </p>
+        <img src="//unsplash.it/506" alt="" />
+    </div>
+</div>
+<br />
+<br />
+<hr />
+<h1>Without Subgrid</h1>
+<br />
+<br />
+<h3>
+    Without subgrid, each card child grid is independent of the parent grid.
+    Meaning that the images all have different widths depending on the window
+    resizing.
+</h3>
+<div class="card-grid">
+    <div class="card">
+        <h2>Lorem ipsum</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            recusandae maxime distinctio quos ullam incidunt.
+        </p>
+        <img src="//unsplash.it/501" alt="" />
+    </div>
+    <div class="card">
+        <h2>Omnis, veritatis odio.</h2>
+        <p>
+            Enim molestiae recusandae ut dolor sit amet consectetur adipisicing.
+            odit possimus eius inventore. Quasi illo unde neque? Itaque,
+            deleniti adipisci?
+        </p>
+        <img src="//unsplash.it/502" alt="" />
+    </div>
+    <div class="card">
+        <h2>Exercitationem, libero quam!</h2>
+        <p>
+            Error maiores culpa eaque. Quam quisquam quae nostrum ipsa dolorum
+            atque aperiam fugit soluta error!
+        </p>
+        <img src="//unsplash.it/503" alt="" />
+    </div>
+    <div class="card">
+        <h2>Quibusdam, recusandae odio.</h2>
+        <p>
+            Corporis laboriosam, neque est commodi architecto voluptatem, ipsam
+            corrupti ullam similique eligendi hic qui natus?
+        </p>
+        <img src="//unsplash.it/504" alt="" />
+    </div>
+    <div class="card">
+        <h2>Modi, exer citat ionem dicta.</h2>
+        <p>
+            Laboriosam, aliquam tempore minus dolorem ullam et veniam
+            asperiores, eveniet vitae odit itaque eligendi ducimus?
+        </p>
+        <img src="//unsplash.it/505" alt="" />
+    </div>
+    <div class="card">
+        <h2>Dolore.</h2>
+        <p>
+            Reprehenderit, labore. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Sunt, aut. harum vitae voluptate alias saepe
+            debitis quo perspiciatis, delectus fugiat modi eveniet.
+        </p>
+        <img src="//unsplash.it/506" alt="" />
+    </div>
+</div>
+
+<!-- CSS Before -->
+<!-- <style>
+    body {
+        font-size: 1.125rem;
+        line-height: 1.5;
+    }
+
+    .card-grid {
+        display: grid;
+        gap: 2rem;
+        grid-template-columns: 1fr 1fr;
+
+        /*   other styling */
+        width: min(90%, 60rem);
+        margin-inline: auto;
+    }
+
+    .card {
+        display: grid;
+        grid-template-columns: minmax(6ch, 20ch) 1fr;
+        grid-template-rows: min-content 1fr;
+        gap: 1em;
+        border: 3px solid black;
+    }
+
+    .card h2 {
+        grid-column: 2;
+        padding-top: 1rem;
+        padding-right: 1em;
+        font-size: 1.75rem;
+        line-height: 1;
+    }
+
+    .card p {
+        grid-column: 2;
+        padding-bottom: 1rem;
+        padding-right: 1em;
+    }
+
+    .card img {
+        grid-column: 1;
+        grid-row: 1 / span 2;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    img {
+        max-width: 100%;
+    }
+
+    h2,
+    h3,
+    p {
+        margin: 0;
+    }
+
+    h1 {
+        margin: 2em;
+        text-align: center;
+    }
+</style> -->
+
+<!-- CSS After -->
+<style>
+    :global(html) {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    body {
+        font-size: 1.125rem;
+        line-height: 1.5;
+    }
+
+    img {
+        max-width: 100%;
+    }
+
+    h2,
+    h3,
+    p {
+        margin: 0;
+    }
+
+    h1,
+    h2, 
+    h3{
+        margin: 2rem auto;
+        text-align: center;
+        max-width: 30rem;
+        
+    }
+
+    .with-subgrid.card-grid {
+        display: grid;
+        gap: 2rem;
+        grid-template-columns:
+            minmax(6ch, 20ch) 1fr
+            minmax(6ch, 20ch) 1fr; /* MODIFIED */
+
+        /*   other styling */
+        width: min(90%, 60rem);
+        margin-inline: auto;
+    }
+
+    .with-subgrid .card {
+        grid-column: span 2; /* ADDED */
+        grid-template-columns: subgrid; /* ADDED */
+
+        display: grid;
+        /* grid-template-columns: minmax(6ch, 20ch) 1fr; */ /* DISABLED */
+        grid-template-rows: min-content 1fr;
+        gap: 1em;
+        border: 3px solid black;
+    }
+
+    .with-subgrid .card h2 {
+        grid-column: 2;
+        padding-top: 1rem;
+        padding-right: 1em;
+        font-size: 1.75rem;
+        line-height: 1;
+    }
+
+    .with-subgrid .card p {
+        grid-column: 2;
+        padding-bottom: 1rem;
+        padding-right: 1em;
+    }
+
+    .with-subgrid .card img {
+        grid-column: 1;
+        grid-row: 1 / span 2;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .card-grid {
+        display: grid;
+        gap: 2rem;
+        grid-template-columns: 1fr 1fr;
+
+        /*   other styling */
+        width: min(90%, 60rem);
+        margin-inline: auto;
+    }
+
+    .card {
+        display: grid;
+        grid-template-columns: minmax(6ch, 20ch) 1fr;
+        grid-template-rows: min-content 1fr;
+        gap: 1em;
+        border: 3px solid black;
+    }
+
+    .card h2 {
+        grid-column: 2;
+        padding-top: 1rem;
+        padding-right: 1em;
+        font-size: 1.75rem;
+        line-height: 1;
+    }
+
+    .card p {
+        grid-column: 2;
+        padding-bottom: 1rem;
+        padding-right: 1em;
+    }
+
+    .card img {
+        grid-column: 1;
+        grid-row: 1 / span 2;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+</style>
